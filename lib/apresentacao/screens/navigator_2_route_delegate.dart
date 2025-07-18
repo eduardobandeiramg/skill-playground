d4bb77fe7fae0e60b7d975ee082d0bc921f17307
eduardobandeiramg <eduardobandeiramg@gmail.com>
2025-07-18 09:05:24 -0300

    iniciando implementacao da tela 2

---
+++ lib/apresentacao/screens/navigator_2_route_delegate.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Navigator2RouteDelegate extends StatelessWidget {
+  const Navigator2RouteDelegate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(pages: [],);
+  }
+}
--- lib/dominio/utils/indice_botoes.dart
+++ lib/dominio/utils/indice_botoes.dart
@@ -1,6 +1,7 @@
-// enum para pré-listar todas as telas do app e definir o tipo do botão na construção do seu objeto
 import 'package:skill_playground/apresentacao/screens/custom_painter_screen.dart';
+import 'package:skill_playground/apresentacao/screens/navigator_2_route_delegate.dart';
 
+// enum para pré-listar todas as telas do app e definir o tipo do botão na construção do seu objeto
 enum Botao {
   customPainterBasics,
   navigator,
@@ -11,20 +12,28 @@ enum Botao {
   animationControllerLifeCycle,
   streamBuilderErrorHandling,
   customSlivers,
-  dependencyInjection
+  dependencyInjection,
 }
 
 // Mapa com informações sobre o título do botão e a tela para o qual ele vai navegar
 Map<Botao, dynamic> infosBotao = {
   Botao.customPainterBasics: {"titulo": "CustomPainter Basics", "tela": CustomPainterScreen()},
-  Botao.navigator : {"titulo": "Navigator 2.0/Router Delegate", "tela": CustomPainterScreen()},
-  Botao.blocStateManagement : {"titulo": "Bloc State Management", "tela": CustomPainterScreen()},
-  Botao.platformChannels : {"titulo": "Platform Channels", "tela": CustomPainterScreen()},
-  Botao.performanceAndRepaints : {"titulo": "Performance & Repaints", "tela": CustomPainterScreen()},
-  Botao.asyncIsolates : {"titulo": "Async Isolates", "tela": CustomPainterScreen()},
-  Botao.animationControllerLifeCycle : {"titulo": "AnimationController LifeCycle", "tela": CustomPainterScreen()},
-  Botao.streamBuilderErrorHandling : {"titulo": "StreamBuilder Error Handling", "tela": CustomPainterScreen()},
-  Botao.customSlivers : {"titulo": "Custom Slivers", "tela": CustomPainterScreen()},
-  Botao.dependencyInjection : {"titulo": "Dependency Injection (get_it)", "tela": CustomPainterScreen()},
-
+  Botao.navigator: {"titulo": "Navigator 2.0/Router Delegate", "tela": Navigator2RouteDelegate()},
+  Botao.blocStateManagement: {"titulo": "Bloc State Management", "tela": CustomPainterScreen()},
+  Botao.platformChannels: {"titulo": "Platform Channels", "tela": CustomPainterScreen()},
+  Botao.performanceAndRepaints: {"titulo": "Performance & Repaints", "tela": CustomPainterScreen()},
+  Botao.asyncIsolates: {"titulo": "Async Isolates", "tela": CustomPainterScreen()},
+  Botao.animationControllerLifeCycle: {
+    "titulo": "AnimationController LifeCycle",
+    "tela": CustomPainterScreen(),
+  },
+  Botao.streamBuilderErrorHandling: {
+    "titulo": "StreamBuilder Error Handling",
+    "tela": CustomPainterScreen(),
+  },
+  Botao.customSlivers: {"titulo": "Custom Slivers", "tela": CustomPainterScreen()},
+  Botao.dependencyInjection: {
+    "titulo": "Dependency Injection (get_it)",
+    "tela": CustomPainterScreen(),
+  },
 };
