0f1a1dac47d171e023ed18317f3f15727076f808
eduardobandeiramg <eduardobandeiramg@gmail.com>
2025-07-20 15:51:25 -0300

    transformando tela stateful em stateless

--- lib/apresentacao/screens/primary_screens/9_stream_builder/stream_builder_screen.dart
+++ lib/apresentacao/screens/primary_screens/9_stream_builder/stream_builder_screen.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:skill_playground/dados/Streams/simulador_stream.dart';
 import 'package:skill_playground/utils/dimensoes.dart';
 
-class StreamBuilderErrorHandling extends StatefulWidget {
+class StreamBuilderErrorHandling extends StatelessWidget {
   const StreamBuilderErrorHandling({super.key});
 
-  @override
-  State<StreamBuilderErrorHandling> createState() => _StreamBuilderErrorHandlingState();
-}
-
-class _StreamBuilderErrorHandlingState extends State<StreamBuilderErrorHandling> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +42,7 @@ class _StreamBuilderErrorHandlingState extends State<StreamBuilderErrorHandling>
               ),
             );
           } else if (snapshot.hasData) {
-            if (snapshot.data!.isEmpty) {
+            /*            if (snapshot.data!.isEmpty) {
               return Center(
                 child: Column(
                   children: [
@@ -60,20 +55,19 @@ class _StreamBuilderErrorHandlingState extends State<StreamBuilderErrorHandling>
                   ],
                 ),
               );
-            } else {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.shopping_cart, color: Colors.red, size: Dimensoes.altura * 0.1),
-                    Text(
-                      "Comprar ${snapshot.data}",
-                      style: TextStyle(fontSize: Dimensoes.altura * 0.04),
-                    ),
-                  ],
-                ),
-              );
-            }
+            } */
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.shopping_cart, color: Colors.red, size: Dimensoes.altura * 0.1),
+                  Text(
+                    "Comprar ${snapshot.data}",
+                    style: TextStyle(fontSize: Dimensoes.altura * 0.04),
+                  ),
+                ],
+              ),
+            );
           } else {
             return Center(
               child: Column(
